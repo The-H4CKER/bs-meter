1312d0609d916f17b383744e6827c2673e3a124e
Nigel Jacob <nigel.a.jacob@gmail.com>
2025-02-15 15:06:35 +0000

    Uploaded dataset processing

---
+++ data_processing.py
@@ -0,0 +1,35 @@
+import os
+from pandas import DataFrame
+from sklearn.feature_extraction.text import TfidfVectorizer, ENGLISH_STOP_WORDS
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from xgboost import XGBClassifier
+
+nature_dir = "dataset/nature_articles/"
+chatgpt_dir = "dataset/chatgpt_articles/"
+
+
+def load_articles_from_folder(folder, label):
+    articles = []
+    for filename in os.listdir(folder):
+        filepath = os.path.join(folder, filename)
+        with open(filepath, "r", encoding="utf-8") as file:
+            text = file.read()
+            articles.append((text, label))
+    return articles
+
+
+nature_articles = load_articles_from_folder(nature_dir, 0)
+chatgpt_articles = load_articles_from_folder(chatgpt_dir, 1)
+
+
+data = nature_articles + chatgpt_articles
+df = DataFrame(data, columns=["text", "label"])
+
+
+custom_stop_word_list = list(ENGLISH_STOP_WORDS.union({"figure", "fig"}))
+vectorizer = TfidfVectorizer(stop_words=custom_stop_word_list)
+
+
+X = vectorizer.fit_transform(df["text"])
+y = df["label"]
