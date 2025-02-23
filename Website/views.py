85adae2a13a3fb8d936db0e0194f8e3c6e873ae6
Wei Heng <whw28@cam.ac.uk>
2025-02-23 16:35:07 +0000

    Website push3 changes

--- Website/views.py
+++ Website/views.py
@@ -1,14 +1,15 @@
-from flask import Flask
+from flask import Flask, Blueprint, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
-from flask import Blueprint, render_template
-
 views = Blueprint("views", __name__)
+
 
 @views.route("/")
 def home():
     return render_template("index.html")
+
+
+@views.route("/about")
+def about():
+    return render_template("about.html")
